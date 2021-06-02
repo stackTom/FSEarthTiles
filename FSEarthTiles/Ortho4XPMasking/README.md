85eff842e0512411b6cc88a031e83e9b998d76ed
stackTom <stackTom@users.noreply.github.com>
2021-06-02 11:47:54 -0400

    add files to allow for creation of mesh using ortho4xp

---
+++ FSEarthTiles/Ortho4XPMasking/README.md
@@ -0,0 +1,7 @@
+# Generate mesh data using Ortho4XP API
+
+This folder contains createMesh.py. This file can be compiled into an exe using pyinstaller as follows:
+
+`pyinstaller --clean -F -p src createMesh.py`.
+
+Note: The src/ folder from Ortho4XP_FSX_P3D (https://github.com/stackTom/Ortho4XP_FSX_P3D) must be present in the same folder as createMesh.py (or, just download Ortho4XP_FSX_P3D, place createMesh.py into the folder containing src/, and use pyinstaller as above).
---
+++ FSEarthTiles/Ortho4XPMasking/createMesh.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+# needed by exe to find all the files to "compile"
+Ortho4XP_dir = "."
+sys.path.append(os.path.join(Ortho4XP_dir, "src"))
+
+# set up working folders
+import O4_File_Names as FNAMES
+FNAMES.Ortho4XP_dir = Ortho4XP_dir
+FNAMES.Utils_dir = os.path.join(FNAMES.Ortho4XP_dir, "Utils")
+FNAMES.OSM_dir = os.path.join(sys.argv[3], "OSM_data")
+FNAMES.Elevation_dir = os.path.join(sys.argv[3], "Elevation_data")
+FNAMES.Tile_dir = os.path.join(sys.argv[3], "Tiles", sys.argv[4])
+
+import O4_Vector_Map as VMAP
+import O4_Config_Utils as CFG
+import O4_Mesh_Utils as MESH
+
+def main(argv):
+    tile = CFG.Tile(int(argv[1]), int(argv[2]), FNAMES.Tile_dir)
+    VMAP.build_poly_file(tile)
+    MESH.build_mesh(tile)
+
+if __name__ == "__main__":
+    main(sys.argv)
